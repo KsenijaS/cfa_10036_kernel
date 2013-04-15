8555a601663beace0720360f3e66aa0cf7bdd17c
Maxime Ripard <maxime.ripard@free-electrons.com>
2013-04-15 16:52:04 +0200

    Update the sample client to generate two frequencies and handle errors
    Signed-off-by: Maxime Ripard <maxime.ripard@free-electrons.com>

--- drivers/misc/cfa10049_fiq_client.c
+++ drivers/misc/cfa10049_fiq_client.c
@@ -14,6 +14,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <math.h>
 
 #include <sys/mman.h>
 #include <sys/stat.h>
@@ -23,12 +24,20 @@
 
 #define FIQ_PATH	"/dev/cfa10049_fiq"
 
+#define XP_FREQUENCY	(2000)
+#define XP_PERIOD	(1000000 / XP_FREQUENCY)
+#define XP_HALF_PERIOD	(XP_PERIOD / 2)
+#define YP_FREQUENCY	(2100)
+#define YP_PERIOD	(1000000 / YP_FREQUENCY)
+#define YP_HALF_PERIOD	(YP_PERIOD / 2)
+
 int main(int argc, char *argv[])
 {
 	struct fiq_buffer *fiq_buf;
 	struct fiq_cell *cell;
 	void *addr;
-	unsigned i;
+	unsigned tick, prev_tick;
+	unsigned long prev_clr, prev_set;
 	int fd;
 	int ret = 0;
 
@@ -46,35 +55,47 @@ int main(int argc, char *argv[])
 		goto out_close;
 	}
 
-	printf("Mapped address %p\n", addr);
-
 	fiq_buf = (struct fiq_buffer*)addr;
 
-	printf("Buffer address %p\n", fiq_buf);
+	for (tick = 0;; tick++) {
+		unsigned long cur_set = 0;
+		unsigned long cur_clr = 0;
 
-	for (i = 0;; i++) {
-		printf("I: %d\n", i);
-		int idx = fiq_buf->wr_idx;
-		printf("index: %d\n", idx);
-		printf("buffer size: %lu\n", fiq_buf->size);
-		cell = fiq_buf->data + idx;
+		if (fiq_buf->status == FIQ_STATUS_ERR_URUN) {
+			printf("Underrun, stopping\n");
+			ioctl(fd, FIQ_STOP);
+			ret = -ENOMEM;
+			goto out_munmap;
+		}
 
-		cell->timer = 10;
-		cell->clear = 0;
-		cell->set = 0;
-
-		if (i & 1)
-			cell->clear = 1 << 22;
+		if (fmod(tick, XP_PERIOD) < XP_HALF_PERIOD)
+			cur_clr |= 1 << 21;
 		else
-			cell->set = 1 << 22;
+			cur_set |= 1 << 21;
 
-		fiq_buf->wr_idx++;
+		if (fmod(tick, YP_PERIOD) < YP_HALF_PERIOD)
+			cur_clr |= 1 << 22;
+		else
+			cur_set |= 1 << 22;
+
+		if ((prev_set != cur_set) || (prev_clr != cur_clr)) {
+			cell = fiq_buf->data + fiq_buf->wr_idx++;
+
+			cell->timer = tick - prev_tick;
+			cell->clear = cur_clr;
+			cell->set = cur_set;
+
+			prev_clr = cur_clr;
+			prev_set = cur_set;
+			prev_tick = tick;
+		}
+
 		if (fiq_buf->wr_idx >= fiq_buf->size)
 			fiq_buf->wr_idx = 0;
 
 		/* Once we have filled the buffer enough, we can start
 		   the FIQ */
-		if (i == 1024) {
+		if (tick == 102400) {
 			ret = ioctl(fd, FIQ_START);
 			if (ret) {
 				printf("Couldn't start the FIQ\n");
@@ -84,7 +105,7 @@ int main(int argc, char *argv[])
 	}
 
 out_munmap:
-	munmap(NULL, FIQ_BUFFER_SIZE);
+	munmap(addr, FIQ_BUFFER_SIZE);
 out_close:
 	close(fd);
 out:
